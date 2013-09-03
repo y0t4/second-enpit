e9773d537d103f75e340fdd0db6b5f707f60a7d5
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2013-09-03 05:22:02 +0000

    Generate Person scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SecondEnpit::Application.routes.draw do
+  resources :people
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'people#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
