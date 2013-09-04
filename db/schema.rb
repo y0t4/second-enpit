f942ec3349804bebcb7a68292ed490f4aee0184a
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2013-09-04 15:25:45 +0900

    Add fish

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130903045112) do
+ActiveRecord::Schema.define(version: 20130904061929) do
+
+  create_table "fish", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "people", force: true do |t|
     t.string   "name"
